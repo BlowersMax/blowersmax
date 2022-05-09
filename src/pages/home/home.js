1fb69314986b34bcfd64c6b041a7336cb8e1e545
maxblowers04 <maxblowers04@gmail.com>
2022-05-09 10:48:20 -0700

    chakra

---
+++ src/pages/home/home.js
@@ -0,0 +1,13 @@
+import { Button, Container, VStack } from "@chakra-ui/react";
+
+function Home() {
+    return(
+        <Container>
+            <VStack>
+                <Button>Hello!</Button>
+            </VStack>
+        </Container>
+    );
+}
+
+export default Home;
